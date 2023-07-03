0e084cc9a3d2b46d10f8d462306febaef8b95d10
Rashid <100156047+RashidRMa@users.noreply.github.com>
2023-07-03 17:08:14 +0400

    init

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./navbar.scss"
+import Sidebar from '../sidebar/Sidebar'
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import GroupIcon from '@mui/icons-material/Group';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+import "./sidebar.scss"
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <div className="top"><span className="logo">lamadmin</span></div>
+            <hr />
+            <div className="center">
+                <ul>
+                    <p className="title">MAIN</p>
+                    <li>
+                        <DashboardIcon className='icon' />
+                        <span>Dashbord</span>
+                    </li>
+                    <p className="title">LISTS</p>
+                    <li>
+                        <GroupIcon className='icon' />
+                        <span>Users</span>
+                    </li>
+                    <li>
+                        <InventoryIcon className='icon' />
+                        <span>Products</span>
+                    </li>
+                    <li>
+                        <CreditCardIcon className='icon' />
+                        <span>Orders</span>
+                    </li>
+                    <li>
+                        <LocalShippingIcon className='icon' />
+                        <span>Delivery</span>
+                    </li>
+                    <p className="title">USEFULL</p>
+                    <li>
+                        <BarChartIcon className='icon' />
+                        <span>Stats</span>
+                    </li>
+                    <li>
+                        <NotificationsNoneIcon className='icon' />
+                        <span>Notifications</span>
+                    </li>
+                    <p className="title">SERVICE</p>
+                    <li>
+                        <SettingsSystemDaydreamOutlinedIcon className='icon' />
+                        <span>System Health</span>
+                    </li>
+                    <li>
+                        <PsychologyIcon className='icon' />
+                        <span>Logs</span>
+                    </li>
+                    <li>
+                        <SettingsApplicationsIcon className='icon' />
+                        <span>Settings</span>
+                    </li>
+                    <p className="title">USER</p>
+                    <li>
+                        <AccountBoxIcon className='icon' />
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <LogoutIcon className='icon' />
+                        <span>Logout</span>
+                    </li>
+                </ul>
+            </div>
+            <div className="bottom">
+                <div className='colorOption'></div>
+                <div className='colorOption'></div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Sidebar
