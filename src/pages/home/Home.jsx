2db80562b6320e8ed141ecee417d0d6f29f194ba
Rashid <100156047+RashidRMa@users.noreply.github.com>
2023-07-04 16:56:35 +0400

    feat:"added progressbar"

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,20 +4,25 @@ import Navbar from '../../components/navbar/Navbar'
 import Widget from '../../components/widget/Widget';
 
 import "./home.scss"
+import Chart from '../../components/chart/Chart';
+import Featured from '../../components/featured/Featured';
 
 const Home = () => {
     return (
         <div className='home'>
             <Sidebar />
             <div className="homeContainer">
-            <Navbar />
-            <div className="widgets">
-                <Widget type="user"/>
-                <Widget type="order"/>
-                <Widget type="earning"/>
-                <Widget type="balance"/>
-
-            </div>
+                <Navbar />
+                <div className="widgets">
+                    <Widget type="user" />
+                    <Widget type="order" />
+                    <Widget type="earning" />
+                    <Widget type="balance" />
+                </div>
+                <div className="charts">
+                    <Featured />
+                    <Chart />
+                </div>
             </div>
         </div>
     )
