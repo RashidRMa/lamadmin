48c8300f121a51636295965797583e3b8bea3df5
Rashid <100156047+RashidRMa@users.noreply.github.com>
2023-07-05 17:09:51 +0400

    feat:"single page added"

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import "./single.scss"
+import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Chart from "../../components/chart/Chart";
+import List from "../../components/table/Table";
+import "./single.scss";
 
 const Single = () => {
     return (
-        <div className='single'>Single</div>
-    )
-}
+        <div className="single">
+            <Sidebar />
+            <div className="singleContainer">
+                <Navbar />
+                <div className="top">
+                    <div className="left">
+                        <div className="editButton">Edit</div>
+                        <h1 className="title">Information</h1>
+                        <div className="item">
+                            <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="itemImg" />
+                            <div className="details">
+                                <h1 className="itemTitle">Jane Doe</h1>
+                                <div className="detailItem">
+                                    <span className="itemKey">Email:</span>
+                                    <span className="itemValue">jane@gmail.com</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Phone:</span>
+                                    <span className="itemValue">+1 234 458 </span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Address:</span>
+                                    <span className="itemValue">Elton st. 234</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Country:</span>
+                                    <span className="itemValue">USA</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <Chart aspect={3 / 1} title="User Spendings (Last 6 Months)" />
+                    </div>
+                </div>
+                <div className="bottom">
+                    <h1 className="title">Last Transactions</h1>
+                    <List />
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Single
+export default Single;
