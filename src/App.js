bb926afa4c96518291b1d844d7ce2331d2f87f2a
Rashid <100156047+RashidRMa@users.noreply.github.com>
2023-07-06 16:33:18 +0400

    feat:"add new page added"

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { productInputs, userInputs } from "./formSource";
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Login from "./pages/login/Login";
@@ -15,12 +16,18 @@ function App() {
           <Route path="users">
             <Route index element={<List />} />
             <Route path=":userId" element={<Single />} />
-            <Route path="new" element={<New />} />
+            <Route
+              path="new"
+              element={<New inputs={userInputs} title={"Add New User"} />}
+            />
           </Route>
           <Route path="products">
             <Route index element={<List />} />
             <Route path=":productId" element={<Single />} />
-            <Route path="new" element={<New />} />
+            <Route
+              path="new"
+              element={<New inputs={productInputs} title={"Add New Product"} />}
+            />
           </Route>
         </Route>
       </Routes>
