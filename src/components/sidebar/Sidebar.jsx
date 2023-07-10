dbe46e8b999bd69025d7bbd78af1e323902f2a87
Rashid Mamedov <rashidrma@code.edu.az>
2023-07-10 15:10:04 +0400

    feat:"routers added"

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,89 +1,96 @@
-import React from 'react'
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import GroupIcon from '@mui/icons-material/Group';
-import InventoryIcon from '@mui/icons-material/Inventory';
-import CreditCardIcon from '@mui/icons-material/CreditCard';
-import LocalShippingIcon from '@mui/icons-material/LocalShipping';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined';
-import PsychologyIcon from '@mui/icons-material/Psychology';
-import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import LogoutIcon from '@mui/icons-material/Logout';
+import React from "react";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import GroupIcon from "@mui/icons-material/Group";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import PsychologyIcon from "@mui/icons-material/Psychology";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { Link } from "react-router-dom";
 
-import "./sidebar.scss"
+import "./sidebar.scss";
 
 const Sidebar = () => {
-    return (
-        <div className='sidebar'>
-            <div className="top"><span className="logo">lamadmin</span></div>
-            <hr />
-            <div className="center">
-                <ul>
-                    <p className="title">MAIN</p>
-                    <li>
-                        <DashboardIcon className='icon' />
-                        <span>Dashbord</span>
-                    </li>
-                    <p className="title">LISTS</p>
-                    <li>
-                        <GroupIcon className='icon' />
-                        <span>Users</span>
-                    </li>
-                    <li>
-                        <InventoryIcon className='icon' />
-                        <span>Products</span>
-                    </li>
-                    <li>
-                        <CreditCardIcon className='icon' />
-                        <span>Orders</span>
-                    </li>
-                    <li>
-                        <LocalShippingIcon className='icon' />
-                        <span>Delivery</span>
-                    </li>
-                    <p className="title">USEFULL</p>
-                    <li>
-                        <BarChartIcon className='icon' />
-                        <span>Stats</span>
-                    </li>
-                    <li>
-                        <NotificationsNoneIcon className='icon' />
-                        <span>Notifications</span>
-                    </li>
-                    <p className="title">SERVICE</p>
-                    <li>
-                        <SettingsSystemDaydreamOutlinedIcon className='icon' />
-                        <span>System Health</span>
-                    </li>
-                    <li>
-                        <PsychologyIcon className='icon' />
-                        <span>Logs</span>
-                    </li>
-                    <li>
-                        <SettingsApplicationsIcon className='icon' />
-                        <span>Settings</span>
-                    </li>
-                    <p className="title">USER</p>
-                    <li>
-                        <AccountBoxIcon className='icon' />
-                        <span>Profile</span>
-                    </li>
-                    <li>
-                        <LogoutIcon className='icon' />
-                        <span>Logout</span>
-                    </li>
-                </ul>
-            </div>
-            <div className="bottom">
-                <div className='colorOption'></div>
-                <div className='colorOption'></div>
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">lamadmin</span>
+        </Link>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashbord</span>
+          </li>
+          <p className="title">LISTS</p>
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <GroupIcon className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <Link to="/products" style={{ textDecoration: "none" }}>
+            <li>
+              <InventoryIcon className="icon" />
+              <span>Products</span>
+            </li>
+          </Link>
+          <li>
+            <CreditCardIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFULL</p>
+          <li>
+            <BarChartIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SettingsSystemDaydreamOutlinedIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsApplicationsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountBoxIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
+    </div>
+  );
+};
 
-            </div>
-
-        </div>
-    )
-}
-
-export default Sidebar
+export default Sidebar;
